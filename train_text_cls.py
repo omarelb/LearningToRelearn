e15a052944e96804d76f3ad4a814ae44c62c120f
Omar Elbaghdadi <omarelb@gmail.com>
2021-02-02 14:23:14 +0100

    write results to a file

--- train_text_cls.py
+++ train_text_cls.py
@@ -8,6 +8,7 @@ from datetime import datetime
 import numpy as np
 import torch
 import pandas as pd
+import wandb
 
 # command line management
 from dataclasses import dataclass, field
@@ -15,13 +16,15 @@ import hydra
 from omegaconf import DictConfig, OmegaConf
 
 from MetaLifeLongLanguage.datasets.text_classification_dataset import get_datasets
-from MetaLifeLongLanguage.learner import EXPERIMENT_DIR
+from MetaLifeLongLanguage.learner import EXPERIMENT_DIR, flatten_dict
 from MetaLifeLongLanguage.models.cls_agem import AGEM
 from MetaLifeLongLanguage.models.cls_anml import ANML
 from MetaLifeLongLanguage.models.cls_baseline import Baseline
 from MetaLifeLongLanguage.models.cls_maml import MAML
 from MetaLifeLongLanguage.models.cls_oml import OML
 from MetaLifeLongLanguage.models.cls_replay import Replay
+
+RESULTS_FILE = Path(hydra.utils.to_absolute_path("results.csv"))
 
 logging.basicConfig(level="INFO", format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 logger = logging.getLogger("ContinualLearningLog")
@@ -46,12 +49,25 @@ def get_learner(config, **kwargs):
         raise NotImplementedError
     return learner
 
+def write_results(config, mean_validation_results, mean_test_results):
+    run_info = flatten_dict(config)
+    for key in list(run_info.keys()):
+        if key.startswith("hydra"):
+            del run_info[key]
+    for k, v in list(mean_validation_results.items()) + list(mean_test_results.items()):
+        run_info[k] = v
+    run_info["finish_time"] = datetime.now()
+
+    df = pd.read_csv(RESULTS_FILE) if RESULTS_FILE.exists() else pd.DataFrame()
+    df = df.append(run_info, ignore_index=True)
+    df.to_csv(RESULTS_FILE, index=False)
+
 @hydra.main(config_path="config", config_name="defaults.yaml")
 def main(config):
     # to load a checkpoint and perform inference, add +evaluate=<dir_of_experiment>
     if "evaluate" not in config: 
         learner = get_learner(config)
-        datasets = get_datasets(learner.data_dir, config.data.order)
+        datasets = get_datasets(learner.data_dir, config.data.order, debug=config.debug)
         learner.training(datasets)
 
         learner.save_checkpoint()
@@ -62,17 +78,26 @@ def main(config):
         config.wandb = False
         learner = get_learner(config, experiment_path=experiment_path)
         learner.load_checkpoint()
-        datasets = get_datasets(learner.data_dir, config.data.order)
+        datasets = get_datasets(learner.data_dir, config.data.order, debug=config.debug)
 
     # validation set
     logger.info("----------Validation starts here----------")
-    results = learner.testing(datasets["val"])
+    results, mean_results = learner.testing(datasets["val"])
+    mean_validation_results = {"validation_" + k : v for k, v in mean_results.items()}
     pd.DataFrame.from_dict(results, orient="index").to_csv(learner.results_dir / "validation_results.csv")
 
     # test set
     logger.info("----------Testing starts here----------")
-    results = learner.testing(datasets["test"])
+    results, mean_results = learner.testing(datasets["test"])
+    mean_test_results = {"test_" + k : v for k, v in mean_results.items()}
     pd.DataFrame.from_dict(results, orient="index").to_csv(learner.results_dir / "test_results.csv")
+
+    if config.wandb:
+        wandb.log(mean_validation_results)
+        wandb.log(mean_test_results)
+
+    # write results to a file
+    write_results(config, mean_validation_results, mean_test_results)
 
 if __name__ == "__main__":
     main()
